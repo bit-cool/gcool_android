07b80dad84c4e909a7c2d47e3ae905e716b4746e
flyingharry <flyingharry@163.com>
2016-05-06 23:01:27 +0800

    -

--- src/main/java/com/example/lenovo/myapplication/activity/Me.java
+++ src/main/java/com/example/lenovo/myapplication/activity/Me.java
@@ -1,26 +1,71 @@
 package com.example.lenovo.myapplication.activity;
 
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.lenovo.myapplication.R;
+import com.example.lenovo.myapplication.adapter.MeAdapter;
+
+import java.util.ArrayList;
 
 public class Me extends AppCompatActivity {
     ViewPager viewPager;
+    LocalActivityManager manager;
+    TextView Edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
+        manager=new LocalActivityManager(this,true);
+        manager.dispatchCreate(savedInstanceState);
+        initViews();
+
+    }
+
+    private void initViews() {
+
+        Edit=(TextView)findViewById(R.id.me_edit);
+        Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getApplicationContext(),Me_Edit.class);
+                startActivity(intent);
+            }
+        });
+
+
         viewPager=(ViewPager)findViewById(R.id.me_vp);
+        Intent in1=new Intent(getApplicationContext(), Me_Opus.class);
+        View view1=getView("",in1);
+        Intent in2=new Intent(getApplicationContext(), Me_Collections.class);
+        View view2=getView("",in2);
+        Intent in3=new Intent(getApplicationContext(), Me_Amds.class);
+        View view3=getView("",in3);
+        ArrayList<View> list=new ArrayList<>();
+        list.add(view1);
+        list.add(view2);
+        list.add(view3);
+        MeAdapter adapter=new MeAdapter(list);
+        viewPager.setAdapter(adapter);
+
         TabLayout tabLayout= (TabLayout)findViewById(R.id.me_tabLayout);
 
-//        tabLayout.setupWithViewPager(viewPager);
-//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-//        tabLayout.setTabMode(TabLayout.MODE_FIXED);
-//        tabLayout.getTabAt(0).setText("我的作品");
-//        tabLayout.getTabAt(1).setText("我的收藏");
-//        tabLayout.getTabAt(2).setText("我的AMD");
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.getTabAt(0).setText("我的作品");
+        tabLayout.getTabAt(1).setText("我的收藏");
+        tabLayout.getTabAt(2).setText("我的AMD");
+    }
+
+    private View getView(String id, Intent intent){
+        return manager.startActivity(id,intent).getDecorView();
     }
 }
---
+++ src/main/java/com/example/lenovo/myapplication/activity/Me_Collections.java
@@ -0,0 +1,82 @@
+package com.example.lenovo.myapplication.activity;
+
+import android.graphics.Canvas;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+
+import com.example.lenovo.myapplication.R;
+import com.example.lenovo.myapplication.adapter.RecyclerAdapter;
+import com.example.lenovo.myapplication.object.VideoInfo;
+
+import java.util.ArrayList;
+
+public class Me_Collections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me__collections);
+        initViews();
+    }
+    private void initViews() {
+        RecyclerView recyclerView;
+        RecyclerAdapter recyclerAdapter;
+        recyclerView=(RecyclerView)findViewById(R.id.me_collections_rv);
+        recyclerAdapter=new RecyclerAdapter(getApplicationContext(),getVideoInfos());
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));  //设置RecyclerView布局管理器为2列垂直排布
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDraw(c, parent, state);
+            }
+        });
+        recyclerView.setAdapter(recyclerAdapter);
+        //这里实现点击视频事件
+        recyclerAdapter.setOnClickListener(new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int postion) {
+
+            }
+
+            @Override
+            public void onItemLongClick(View view, int postion) {
+
+            }
+        });
+    }
+    /*
+   * 图片高度从服务器获得
+   * */
+    private ArrayList<VideoInfo> getVideoInfos(){
+        ArrayList<VideoInfo> data=new ArrayList<>();
+        VideoInfo videoInfo=new VideoInfo();
+        videoInfo.setImgHeight(1000);
+        videoInfo.setUserName("flyingharry");
+        videoInfo.setImag(R.drawable.fenlei_01);
+
+        VideoInfo videoInfo1=new VideoInfo();
+        videoInfo1.setUserName("happen");
+        videoInfo1.setImgHeight(800);
+        videoInfo1.setImag(R.drawable.fenlei_02);
+
+        VideoInfo videoInfo2=new VideoInfo();
+        videoInfo2.setUserName("happen");
+        videoInfo2.setImgHeight(900);
+        videoInfo2.setImag(R.drawable.fenlei_03);
+
+        data.add(videoInfo);
+        data.add(videoInfo1);
+        data.add(videoInfo);
+        data.add(videoInfo2);
+        data.add(videoInfo1);
+        data.add(videoInfo2);
+        data.add(videoInfo1);
+        return data;
+    }
+}
